5a1de90c41dc65faa5a946d8270863cfb7f5775e
Jay Robohn <jayrobot@gmail.com>
2025-03-17 11:47:11 -0400

    :recycle: refactor(pages) Implemented env vars to handle Google Maps API Key

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.healthyfamiliesnewyork.org',
   base: '/',
   output: 'static',
+
   build: {
     assets: '_assets'
   },
+
+  env: {
+    schema: { 
+      SECRET_GOOGLEMAPSAPIKEY: envField.string({
+        context: 'server',
+        access: 'secret',
+        description: 'Google Maps API Key',        
+      }),
+    }
+  },
+
   vite: {
     build: {
       assetsInlineLimit: 0
