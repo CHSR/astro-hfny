d978b9f18a9802703c384877e75306e83be43331
Jay Robohn <jayrobot@gmail.com>
2025-03-24 10:02:18 -0400

    ♻️ chore(comp) Reformatted accordion code

--- public/js/accordion.js
+++ public/js/accordion.js
@@ -1,28 +1,24 @@
 // JavaScript Document
 
-
-  $(document).ready(function($) {
-    $('#accordion').find('.accordion-toggle').click(function(){
-
+$(document).ready(function ($) {
+  $("#accordion")
+    .find(".accordion-toggle")
+    .click(function () {
       //Expand or collapse this panel
-      $(this).next().slideToggle('fast');
+      $(this).next().slideToggle("fast");
 
       //Hide the other panels
-      $(".accordion-content").not($(this).next()).slideUp('fast');
-
+      $(".accordion-content").not($(this).next()).slideUp("fast");
     });
-  });
-  
-  $("#expandAll").click(function(){
-    $(".accordion-content, #collapseAll").show()
-	$("#expandAll").hide()
-
-
 });
 
+$("#expandAll").click(function () {
+  $(".accordion-content, #collapseAll").show();
+  $("#expandAll").hide();
+});
 
-  $("#collapseAll").click(function(){
-    $(".accordion-content").hide()
-    $("#collapseAll").hide()
-    $("#expandAll").show()
+$("#collapseAll").click(function () {
+  $(".accordion-content").hide();
+  $("#collapseAll").hide();
+  $("#expandAll").show();
 });
